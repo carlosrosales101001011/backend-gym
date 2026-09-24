d7f05b77713b95357a1fec3baa5e4c4fc55c408b
carlosrosales101001011 <carlosrosales21092002@hotmail.com>
2026-09-24 16:03:01 -0500

    puerto o 3k

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    origin: ['http://localhost:5174', 'http://localhost:5173', 'https://frontend-gym-mu.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // si usas cookies o auth
   })
@@ -18,6 +18,7 @@ async function bootstrap() {
     }),
     
   );
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  console.log(`Servidor ejecutándose en puerto ${process.env.PORT || 3000}`);
 }
 bootstrap();
